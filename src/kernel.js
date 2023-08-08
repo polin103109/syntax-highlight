fd048e019ba96254dbf75a3ae978ba2c4742199b
polin103109 <sabrinpolinece18@gmail.com>
2023-08-08 07:14:48 +0600

    added color pick

--- src/kernel.js
+++ src/kernel.js
@@ -7,5 +7,16 @@ export const kernelFunction = function (width, height, hue) {
   const x = Math.floor(i / 4 - y * width);
   const channel = i % 4;
 
-  
+  const blackValue = 0;
+  const aquaValue = Math.floor((y / height) * 156) + 99; // Persian green calculation
+
+  if (channel === 3) {
+    return 255; // Alpha channel
+  } else if (channel === 0) {
+    return blackValue; // Red channel (black)
+  } else if (channel === 1) {
+    return blackValue; // Green channel (black)
+  } else if (channel === 2) {
+    return aquaValue; // Blue channel (aqua)
+  }
 };
